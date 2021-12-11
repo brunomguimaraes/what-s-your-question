65689c8e95859868956dc80c2f6d6ab11c862aec
Moisés Brandão <moisesbrandao1998@gmail.com>
2021-12-11 17:45:50 -0300

    created get question by id endpoint

--- src/routers/questionRouter.ts
+++ src/routers/questionRouter.ts
@@ -6,5 +6,6 @@ const router = express.Router();
 router.post('/', questionsController.postQuestion);
 router.post('/:id', questionsController.postAnswer);
 router.get('/', questionsController.getUnansweredQuestions);
+router.get('/:id', questionsController.getQuestionById);
 
 export default router;
